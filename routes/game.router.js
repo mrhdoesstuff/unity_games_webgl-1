c36777c2a756be1b4174021605582934c61b0d69
y-pan <panyunkui@gmail.com>
2018-04-02 16:57:21 -0400

    fixing

--- routes/game.router.js
+++ routes/game.router.js
@@ -3,11 +3,12 @@ const gameRouter = express.Router();
 module.exports = gameRouter; 
 
 gameRouter.get('/lab5', (req, res)=>{
+    console.log("play lab5 ...")
     res.render('lab5/index.html');
 });
 
-
-gameRouter.get('/timelinetrouble/alpha', (req, res)=>{
+gameRouter.get('/timelinetrouble_alpha', (req, res)=>{
+    console.log("play timelinetrouble_alpha ...")
     res.render('TimelineTrouble_ALPHA/index.html');
 });
 
