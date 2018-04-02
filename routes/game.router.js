94ac71a5350265cc37c4c797bf22fd3880ea3dbf
y-pan <panyunkui@gmail.com>
2018-04-02 15:29:58 -0400

    Added TimelineTrouble alpha build

--- routes/game.router.js
+++ routes/game.router.js
@@ -5,3 +5,8 @@ module.exports = gameRouter;
 gameRouter.get('/lab5', (req, res)=>{
     res.render('lab5/index.html');
 });
+
+
+gameRouter.get('/timelinetrouble/alpha', (req, res)=>{
+    res.render('TimelineTrouble_ALPHA/index.html');
+});
