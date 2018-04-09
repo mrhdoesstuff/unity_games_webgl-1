4a15638b6dc7b760af04251892857867a051f0ba
Yun Kui Pan <panyunkui@gmail.com>
2018-04-09 09:51:11 -0400

    adding newer version

--- routes/game.router.js
+++ routes/game.router.js
@@ -6,10 +6,23 @@ gameRouter.get('/lab5', (req, res)=>{
     console.log("play lab5 ...")
     res.render('lab5/index.html');
 });
+gameRouter.get('/lab5b', (req, res)=>{
+    console.log("play lab5 ...")
+    res.sendFile('lab5/index.html');
+});
 
 gameRouter.get('/timelinetrouble', (req, res)=>{
     console.log("play timelinetrouble ...")
     res.render('timelinetrouble/index.html');
+});
+
+gameRouter.get('/timelinetrouble2', (req, res)=>{
+    console.log("play timelinetrouble2 ...")
+    res.render('timelinetrouble2/index.html');
+});
+gameRouter.get('/timelinetrouble2b', (req, res)=>{
+    console.log("play timelinetrouble2 ...")
+    res.sendFile('timelinetrouble2/index.html');
 });
 
 gameRouter.get('/', (req, res)=>{
