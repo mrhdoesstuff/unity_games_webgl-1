9e2e1dd09c8474ad17c965ce7ccdf649abc394b7
Yun Kui Pan <panyunkui@gmail.com>
2018-04-11 16:59:42 -0400

    add build 3

--- routes/game.router.js
+++ routes/game.router.js
@@ -6,10 +6,6 @@ gameRouter.get('/lab5', (req, res)=>{
     console.log("play lab5 ...")
     res.render('lab5/index.html');
 });
-gameRouter.get('/lab5b', (req, res)=>{
-    console.log("play lab5 ...")
-    res.sendFile('lab5/index.html');
-});
 
 gameRouter.get('/timelinetrouble', (req, res)=>{
     console.log("play timelinetrouble ...")
@@ -20,9 +16,9 @@ gameRouter.get('/timelinetrouble2', (req, res)=>{
     console.log("play timelinetrouble2 ...")
     res.render('timelinetrouble2/index.html');
 });
-gameRouter.get('/timelinetrouble2b', (req, res)=>{
-    console.log("play timelinetrouble2 ...")
-    res.sendFile('timelinetrouble2/index.html');
+gameRouter.get('/timelinetrouble3', (req, res)=>{
+    console.log("play timelinetrouble3 ...")
+    res.render('timelinetrouble3/index.html');
 });
 
 gameRouter.get('/', (req, res)=>{
